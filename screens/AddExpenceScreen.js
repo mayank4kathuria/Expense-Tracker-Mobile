013288a49f7db9a84008c5c73e844c4e0b776256
Mayank Kathuria <mayank2coolz@gmail.com>
2018-11-02 22:46:09 +0530

    Fixed Typo

--- screens/AddExpenceScreen.js
+++ screens/AddExpenceScreen.js
@@ -1,16 +1,16 @@
 // Add Expense Screen
 
 import React from 'react';
-import { Text , TouchableHilight} from 'react-native';
+import { Text , TouchableHighlight} from 'react-native';
 
 
 export default class AddExpenseScreen{
 
 	render(){
 		return (
-			<TouchableHilight>
+			<TouchableHighlight>
 				<Text>Add Expense Screen Coming Soon!</Text>
-			</TouchableHilight>
+			</TouchableHighlight>
 			);
 	}
 }
