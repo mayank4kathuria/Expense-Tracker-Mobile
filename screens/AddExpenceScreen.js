0fe16f12d36903748cce20b959b43e7870d99401
Mayank Kathuria <mayank2coolz@gmail.com>
2018-11-02 22:38:17 +0530

    Replaced View component with TouchableHighlight

---
+++ screens/AddExpenceScreen.js
@@ -0,0 +1,16 @@
+// Add Expense Screen
+
+import React from 'react';
+import { Text , TouchableHilight} from 'react-native';
+
+
+export default class AddExpenseScreen{
+
+	render(){
+		return (
+			<TouchableHilight>
+				<Text>Add Expense Screen Coming Soon!</Text>
+			</TouchableHilight>
+			);
+	}
+}
